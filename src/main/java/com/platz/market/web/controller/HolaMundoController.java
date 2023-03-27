251fd90f302de77f53f97cdc5d2c567f889944e7
Gerson Ep <gerson21millones@gmail.com>
2023-03-26 22:51:27 -0600

    10 Config y Estructura de la app

--- src/main/java/com/platz/market/web/controller/HolaMundoController.java
+++ src/main/java/com/platz/market/web/controller/HolaMundoController.java
@@ -1,4 +1,4 @@
-package com.platz.market;
+package com.platz.market.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
