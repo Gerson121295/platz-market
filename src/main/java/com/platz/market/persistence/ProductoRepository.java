59639889003bf00a4d96bb0e6fd15a06f4443ebb
Gerson Ep <gerson21millones@gmail.com>
2023-03-27 23:21:37 -0600

    19 Query Methods y ejemplo de Query Nativo(consultas SQL)

--- src/main/java/com/platz/market/persistence/ProductoRepository.java
+++ src/main/java/com/platz/market/persistence/ProductoRepository.java
@@ -4,6 +4,7 @@ import com.platz.market.persistence.crud.ProductoCrudRepository;
 import com.platz.market.persistence.entity.Producto;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
@@ -13,4 +14,17 @@ public class ProductoRepository {
     public List<Producto> getAll(){
         return (List<Producto>) productoCrudRepository.findAll();
     }
+
+    //Metodo que retorne una lista de producto que pertenece a una categoria ordenado por alfabeto(ascendente)
+    public List<Producto> getByCategoria(int idCategoria){
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);  //usa el Query methods findByIdCategoriaOrderByNombreAsc de la interfaz ProductoCrudRepository
+
+    }
+
+    //Metodo que retorne un optional de una lista de producto de escasos
+
+    public Optional<List<Producto>> getEscasos(int cantidad){ //el parametro , boolean estado no fue necesario porque se le envio true directamente en el cuerpo como argumento(sig, linea de codigo).
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
 }
--- src/main/java/com/platz/market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/platz/market/persistence/crud/ProductoCrudRepository.java
@@ -3,6 +3,9 @@ package com.platz.market.persistence.crud;
 import com.platz.market.persistence.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 /*
 Repositorios de Spring Data
 CrudRepository: Permite hacer las operaciones de CRUD.
@@ -13,6 +16,18 @@ JPARepository: Nos permite hacer lo mismo que los dos anteriores, pero además n
 
 
 public interface ProductoCrudRepository extends CrudRepository <Producto, Integer>{ //recibe 2 parametros: Producto (la tabla), Integer(el tipo de dato de clave primaria)
+
+    //Query Methods
+    //Recuperar toda la lista de productos que pertenezca con una categoria
+     List<Producto>findByIdCategoriaOrderByNombreAsc(int idCategoria);   //buscar los productos por el atributo: idCategoria, hay que respetata el camelCase: se escribe "IdCategoria", y como parametro recibe el idCategoria(como se llama dentro de la clase)
+
+    //Query de manera nativa
+   // @Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true)       //Otra forma es hacerlo por un Query Nativo
+    //Si tenemos Asignado nuestro anotacion Query no es Necesario llamar asi(como el method de arriba) nuestro metodo asi lo podemos llamar de cualquier manera
+    //List<Producto> getByCategoria(int idCategoria);   //buscar los productos por el atributo: idCategoria, hay que respetata el camelCase: se escribe "IdCategoria", y como parametro recibe el idCategoria(como se llama dentro de la clase)
+
+    //Query Methods: Recupera productos por la cantidad de Stock, productos agotados
+    Optional<List<Producto>>findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);  //cantidadStock respentando camel case es CantidadStock, LessThan(menos que), And("y" para agregar mas consulta) Estado(estado en la clase)
 
 
 }
